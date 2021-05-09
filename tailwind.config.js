d5057d63d9e64b9a4a994245575a47780613d927
Luke Bennett <hello@lukebennett.com.au>
2021-05-09 17:16:13 +1000

    Update typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-extraneous-dependencies, sonarjs/no-duplicate-string */
 const { teal, orange } = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
@@ -15,6 +15,45 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'h1, h2, h3, h4': {
+              fontWeight: theme('fontWeight.medium'),
+            },
+            blockquote: {
+              borderColor: theme('colors.teal.600'),
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+            'h1, h2, h3, h4, a, strong, blockquote, code, thead, pre code, a code': {
+              color: theme('colors.gray.100'),
+            },
+            '[class~="lead"], figure figcaption': {
+              color: theme('colors.gray.400'),
+            },
+            blockquote: {
+              borderColor: theme('colors.orange.400'),
+            },
+            pre: {
+              backgroundColor: theme('colors.gray.900'),
+            },
+            ol: {
+              'li::before': {
+                color: theme('colors.orange.400'),
+              },
+            },
+            ul: {
+              'li::before': {
+                backgroundColor: theme('colors.orange.400'),
+              },
+            },
+          },
+        },
+      }),
     },
   },
   variants: {
