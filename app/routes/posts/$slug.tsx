08947451c499c9a280422192d145e883c34cc86f
Luke Bennett <hello@lukebennett.com.au>
2021-05-12 09:23:26 +1000

    Add Sanity content to home page and blog posts

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,35 @@
+import PortableText from 'react-portable-text';
+import type { LoaderFunction } from 'remix';
+import { useRouteData } from 'remix';
+
+import { SanityPost } from '../../../types/sanity';
+import { sanityClient } from '../../utils/sanity-client';
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const { slug = '' } = params;
+  let response: SanityPost | undefined;
+  try {
+    response = await sanityClient.fetch(
+      `*[_type == "post" && slug.current == $slug][0]`,
+      { slug }
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
+  return response;
+};
+
+export default function TeamMember(): JSX.Element | null {
+  const post = useRouteData<SanityPost>();
+  if (!post) return null;
+  return (
+    <div className="max-w-5xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:py-32 lg:px-8">
+      <h1 className="text-4xl">{post.title}</h1>
+      <PortableText
+        content={post.body}
+        serializers={{}}
+        className="mt-12 prose dark:prose-dark"
+      />
+    </div>
+  );
+}
---
+++ types/sanity.ts
@@ -0,0 +1,14 @@
+export interface SanityPost {
+  _createdAt: Date;
+  _id: string;
+  _rev: string;
+  _type: 'post';
+  _updatedAt: Date;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  body: [object];
+  slug: {
+    _type: string;
+    current: string;
+  };
+  title: string;
+}
