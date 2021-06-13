a78e07348c465bcb8ae2d45b2a157685580644e9
Luke Bennett <hello@lukebennett.com.au>
2021-06-13 20:21:16 +1000

    Add light colour border

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -50,7 +50,7 @@ function Posts({ posts }: PostsProps): JSX.Element | null {
             <li key={post._id}>
               <Link
                 to={`/posts/${post.slug.current}/`}
-                className="block px-4 py-6 transition duration-300 ease-in-out rounded-lg transform-gpu dark:border dark:border-orange-400 dark:border-opacity-20 dark:hover:border-opacity-100 sm:px-6 lg:px-8 sm:py-8 lg:py-10 dark:hover:bg-gray-700 hover:shadow-lg hover:-translate-y-1 hover:scale-105"
+                className="block px-4 py-6 transition duration-300 ease-in-out border-2 border-teal-600 rounded-lg transform-gpu dark:border dark:border-orange-400 dark:border-opacity-20 dark:hover:border-opacity-100 sm:px-6 lg:px-8 sm:py-8 lg:py-10 dark:hover:bg-gray-700 hover:shadow-lg hover:-translate-y-1 hover:scale-105"
               >
                 <article className="prose dark:prose-dark">
                   <h2>{post.title}</h2>
